7281dc6d9914dca067b0d73c65c32683cf7a24d0
taarakshukla <taarakshukla01@gmail.com>
2024-07-07 13:23:38 +0530

    f2

--- main.py
+++ main.py
@@ -19,7 +19,7 @@ generation_config = {
     "temperature": 0.9,
     "top_p": 1,
     "top_k": 1,
-    "max_output_tokens": 2049
+    "max_output_tokens": 2048
 }
 model = genai.GenerativeModel("gemini-pro", generation_config=generation_config)
 
